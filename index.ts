d480b96b3a1682c2dea6a53eb039b57c0d782510
Kaan ACAR <umaysoftware@gmail.com>
2025-08-01 00:39:59 +0300

    Remove named export for MediaHelper, retaining only the default export

--- index.ts
+++ index.ts
@@ -287,8 +287,5 @@ export { processAudioFiles } from './src/utils/audio.utils';
 export { processDocumentFiles } from './src/utils/document.utils';
 export { processArchiveFiles } from './src/utils/archive.utils';
 
-// Named export
-export { MediaHelper };
-
 // Default export
 export default MediaHelper;
